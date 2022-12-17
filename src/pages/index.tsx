6037347c6d60e0f73ed3756ced64b4193f41d04e
Umer Farooq <holypulse@gmail.com>
2022-12-17 09:19:00 +0000

    test 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Reporting Tool</h1>
+        <h1 className={styles.title}>Reporting Tool 1</h1>
       </main>
     </div>
   );
